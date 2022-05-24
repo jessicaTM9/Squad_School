c0982fd2b41054c702869c2bf4f52bf02c631145
Silvio Campos <silviocampooss@gmail.com>
2022-05-24 19:06:15 -0300

    fix/routers-to-fake-server

--- src/app/courses/courses.service.ts
+++ src/app/courses/courses.service.ts
@@ -13,24 +13,24 @@ export class CoursesService {
   }
 
   list(): Observable<ICourse[]> { //esse list retorna o tipo observable, dentro dele retorna o array.
-    return this.http.get<ICourse[]>("http://localhost:3000/cursos")    
+    return this.http.get<ICourse[]>("http://localhost:3000/courses")    
   }
 
   add(descricao: string, ementa: string){
-    return this.http.post("http://localhost:3000/cursos",{descricao,ementa})
+    return this.http.post("http://localhost:3000/courses",{descricao,ementa})
 
   }
   edit(codigo: number, descricao: string, ementa:string){
     
-    return this.http.put("http://localhost:3000/cursos/"+codigo,{descricao, ementa})
+    return this.http.put("http://localhost:3000/courses/"+codigo,{descricao, ementa})
 
   }
 
   getByCodigo(codigo: number): Observable<ICourse> { //esse list retorna o tipo observable, dentro dele retorna o array.
-    return this.http.get<ICourse>("http://localhost:3000/cursos/"+codigo)
+    return this.http.get<ICourse>("http://localhost:3000/courses/"+codigo)
   }
   deleteByCodigo(codigo: number) { //esse list retorna o tipo observable, dentro dele retorna o array.
-    return this.http.delete("http://localhost:3000/cursos/"+codigo)
+    return this.http.delete("http://localhost:3000/courses/"+codigo)
   }
 
 }
